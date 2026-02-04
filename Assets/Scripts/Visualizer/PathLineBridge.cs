7ef41e9e9d2af56d8149a66d17cc688df2fc7e9b
AlekseySZ <it.korvis@gmail.com>
2026-02-04 20:51:20 +0300

    feat: add PathLineBridge to visualize players pf path

---
+++ Assets/Scripts/Visualizer/PathLineBridge.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Visualizer
+{
+    public class PathLineBridge : MonoBehaviour
+    {
+        public LineRenderer lineRenderer;
+        public static PathLineBridge Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+            if (lineRenderer == null) lineRenderer = GetComponent<LineRenderer>();
+        }
+        public void UpdatePath(Vector3[] positions)
+        {
+            lineRenderer.enabled = true;
+            lineRenderer.positionCount = positions.Length;
+            lineRenderer.SetPositions(positions);
+        }
+
+        public void ClearPath() => lineRenderer.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Visualizer/PlayerPathLineSystem.cs
@@ -0,0 +1,53 @@
+using Gameplay.Player;
+using PFStar;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace Visualizer
+{
+    [UpdateInGroup(typeof(PresentationSystemGroup))]
+    public partial class PlayerPathLineSystem : SystemBase    {
+        
+        protected override void OnUpdate()
+        {
+
+            if (PathLineBridge.Instance == null) return;
+            bool playerPathActive = false;
+
+            foreach (var (state, transform, entity) in 
+                     SystemAPI.Query<RefRO<PFAgentState>, RefRO<LocalTransform>>()
+                         .WithAll<PlayerTag>()
+                         .WithEntityAccess())
+            {
+                var waypoints = SystemAPI.GetBuffer<Waypoint>(entity);
+
+                if ((state.ValueRO.Flags & (byte)PFAgentStatus.Process) != 0 && waypoints.Length > 0)
+                {
+                    playerPathActive = true;
+                    
+                    Vector3[] positions = new  Vector3[waypoints.Length];
+                    
+                    for (int i = 1; i <= waypoints.Length; i++)
+                    {
+                        float3 wp = waypoints[i-1].point;
+                        
+                        positions[^i] = new Vector3(
+                            wp.x, 
+                            wp.y + 0.1f, 
+                            wp.z  
+                        );
+                    }
+                    
+                    PathLineBridge.Instance.UpdatePath(positions);
+                }
+            }
+
+            if (!playerPathActive)
+            {
+                PathLineBridge.Instance.ClearPath();
+            }
+        }
+    }
+}
